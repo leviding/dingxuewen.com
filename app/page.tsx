de6f6e6d5391c3f6ad1d5dd0b5a5e203c3c15fe3
LeviDing <imdingxuewen@gmail.com>
2023-11-12 15:30:11 +0800

    feat: 处理首页文章点击

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
+'use client';
+
 import Image from 'next/image';
 import { CommonAside, Icon, Link, MainContentContainer, Tag } from '@/components';
 // import siteMetadata from '@/data/siteMetadata';
 import { sortPosts, allCoreContent } from '@/utils/contentlayer';
 import { allPosts } from 'contentlayer/generated';
+import { useRouter } from 'next/navigation';
 
-const MAX_DISPLAY = 5;
+const MAX_DISPLAY = 10;
 
 const Home = () => {
+  const router = useRouter();
   const sortedPosts = sortPosts(allPosts);
   const posts = allCoreContent(sortedPosts);
 
@@ -18,7 +22,10 @@ const Home = () => {
           const { title, date, lastModify, summary, tags, slug, image } = post;
           return (
             <div key={slug}>
-              <div className="flex justify-between px-5 py-4 hover:bg-gray-50 dark:hover:bg-neutral-800 cursor-pointer">
+              <div
+                onClick={() => router.push(`/post/${slug}`)}
+                className="flex justify-between px-5 py-4 hover:bg-gray-50 dark:hover:bg-neutral-800 cursor-pointer"
+              >
                 <div>
                   <Link href={`/post/${slug}`}>
                     <h2 className="text-lg font-bold line-clamp-1 break-all text-gray-900 dark:text-gray-100 mb-1">
--- components/Tag.tsx
+++ components/Tag.tsx
@@ -7,6 +7,7 @@ interface Props {
 const Tag = ({ text }: Props) => {
   return (
     <Link
+      onClick={(e) => e.stopPropagation()}
       href={`/tags/${slug(text)}`}
       className="px-[6px] mt-[6px] mr-2 rounded text-sm bg-gray-100 dark:bg-neutral-700 text-gray-500 dark:text-gray-300  hover:text-brand-hover dark:hover:text-gray-400"
     >
